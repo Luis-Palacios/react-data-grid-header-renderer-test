9f27a7753e5fc469c123d21a71cb041952e76728
Luis Palacios <lrpalaciosdev@gmail.com>
2019-07-11 17:39:08 -0600

    change to sortable false

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const TotalCostHeader = item => (<MyCustomRenderer>{item.column.name}</MyCustomR
 const columns = [
   { key: 'id', name: 'ID' },
   { key: 'title', name: 'Title' },
-  { key: 'count', name: 'Count', headerRenderer: TotalCostHeader, sortable: false } ];
+  { key: 'count', name: 'Count', headerRenderer: TotalCostHeader, sortable: true } ];
 
 const rows = [{id: 0, title: 'row1', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
 
